18e6d2cd1b812fd0a4a071a3894b1d38ef3889d5
Anna Tymotsko <49621867+anliem-tymotsko@users.noreply.github.com>
2020-05-15 13:04:14 +0300

    Create User.js

---
+++ Models/User.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    firstname: {type:String, required:true},
+    lastname: {type:String, required:true},
+    email: {type:String, required:true},
+    password: {type:String, required:true},
+    role: {type:String, require:true}
+})
+
+var User = module.exports = mongoose.model('User', userSchema);
